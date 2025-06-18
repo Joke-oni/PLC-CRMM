17ed4c31ab1563ef5c1e7bc83820c8e732d6613e
Joke-oni <jokeoni@oauife.edu.ng>
2025-06-18 15:33:48 +0100

    Create Node.py

---
+++ Node.py
@@ -0,0 +1,9 @@
+nodes = {
+    "Engineering_PC": {"ip": "192.168.1.5", "type": "workstation", "os": "Windows 10"},
+    "HMI_1": {"ip": "192.168.1.10", "type": "hmi", "vendor": "Siemens"},
+    "PLC_1": {"ip": "192.168.1.20", "type": "plc", "model": "S7-1500", "sil": 3},
+    "PLC_2": {"ip": "192.168.1.30", "type": "plc", "model": "ControlLogix", "sil": 2},
+    "Valve_1": {"ip": "192.168.1.40", "type": "actuator", "safety": "SIL3"},
+    "Motor_1": {"ip": "192.168.1.50", "type": "actuator", "safety": "SIL1"},
+    "Historian": {"ip": "192.168.1.60", "type": "server", "software": "FactoryTalk"}
+}
